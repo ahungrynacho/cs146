55361d6be51e661aa136bef48801296c966c5b75
ahungrynacho <brianbhuynh@gmail.com>
2017-05-24 15:54:14 -0700

    lab5: part 1 final submission

--- lab5/parse.c
+++ lab5/parse.c
@@ -8,79 +8,66 @@ void parse(char * dest, int size, char * buf) {
     commands.num = 0;
     commands.args = NULL;
    
-    char * buf_copy = NULL;
     buf[strlen(buf) - 1] = '\0';    // replace \n with \0
-   
+    char buf_copy[strlen(buf) + 1]; // +1 for the \0 since strcpy appends a \0
     char result[size];
 
-    if (strlen(buf) > 0) {
-        buf_copy = (char *) malloc(sizeof(char) * strlen(buf));
-        strcpy(buf_copy, buf);
-    }
-
+    strcpy(buf_copy, buf);
     char * token = strtok(buf_copy, "|");
-    if (strlen(buf) > 0 && strlen(token) > 1) {        // if the input is not blank
+    if (token != NULL && strlen(token) >= 1) {        // if the input is not blank
         while (token != NULL) {
             ++commands.num;
             token = strtok(NULL, "|");
         }
-        free(buf_copy); 
-      
         
-        buf_copy = (char *) malloc(sizeof(char) * strlen(buf));
         strcpy(buf_copy, buf);
-
         int arg_num = 0;
         token = strtok(buf_copy, " ");
         while (token != NULL) {
             ++arg_num;
             token = strtok(NULL, " ");
         }
-        free(buf_copy);
         
-        buf_copy = (char *) malloc(sizeof(char) * strlen(buf));
         strcpy(buf_copy, buf);
 
-        // allocate array of pointers
-        commands.args = (char **) malloc(sizeof(char *) * arg_num);
-        char ** args = commands.args;
+        // allocate array of pointers on the stack
+        char * args[arg_num];
+        commands.args = args;
         
+        int i = 0;
         token = strtok(buf_copy, " ");
         while (token != NULL) {
-            *args = (char *) malloc(sizeof(char) * strlen(token));
-            strcpy(*args, token);
+            args[i] = (char *) malloc(sizeof(char) * (strlen(token) + 1));    // each element points to an argument
+            strcpy(args[i], token);
             token = strtok(NULL, " ");
-            ++args;
+            ++i;
         }
-        *args = NULL;
-        free(buf_copy);
         
         // concatentate the output to a single buffer    
         sprintf(result, "%d:", commands.num);
-        for (char ** arg = commands.args; *arg != NULL; ++arg) {
-            char buf_cmd[strlen(*arg) * 2];
-            if (strcmp(*arg, "<") == 0 || strcmp(*arg, ">") == 0 || strcmp(*arg, "|") == 0) {
+        for (i = 0; i < arg_num; ++i) {
+            char buf_cmd[strlen(args[i]) * 2];
+            if (strcmp(args[i], "<") == 0 || strcmp(args[i], ">") == 0 || strcmp(args[i], "|") == 0) {
                 // strcat result without single quotes
-                sprintf(buf_cmd, " %s", *arg);
+                sprintf(buf_cmd, " %s", args[i]);
                 strcat(result, buf_cmd);
             }
             else {
                 // strcat result with single quotes
-                sprintf(buf_cmd, " \'%s\'", *arg);
+                sprintf(buf_cmd, " \'%s\'", args[i]);
                 strcat(result, buf_cmd);
             }
         }
-        strcpy(dest, result);
-
-
-        /*        
-        // cleanup
-        for (char ** ptr = commands.args; *ptr != NULL; ++ptr) {
-            free(*ptr);
-        }
-        free(commands.args);
-        */
         
+        // cleanup
+        for (int i = 0; i < arg_num; ++i) {
+            free(args[i]);
+        }
+
     }
+    else {
+        sprintf(result, "0:", NULL);
+    }
+    strcpy(dest, result);
 
 }
